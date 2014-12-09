2ae5bb670019805b46bcea5a58c17826ace579b9
Adam <adwe@live.se>
2014-12-09 19:05:06 +0100

    test 4.3

--- data.c
+++ data.c
@@ -5,15 +5,13 @@ struct test{
   char first;
   double second;
   int third;
-  char fourth;
-  char* fifth;
+  //char fourth;
+  //char* fifth;
 };
 
 int main(int argc, char *argv[])
 {
   int size = sizeof(struct test);
   printf("Size is: %d \n", size);
-  printf("Size is: %d \n", (int)sizeof(double));
-  printf("Size is: %d \n", (int)sizeof(char*));
   return 0;
 }
