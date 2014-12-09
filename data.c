a1bc8156cab8571dcb40421423caa1aba4f065b5
Adam <adwe@live.se>
2014-12-09 19:29:50 +0100

    test 6 again

--- data.c
+++ data.c
@@ -3,7 +3,7 @@
 
 struct test{
   char first;
-  float second;
+  long second;
   int third;
   //char fourth;
   //char* fifth;
@@ -13,6 +13,6 @@ int main(int argc, char *argv[])
 {
   int size = sizeof(struct test);
   printf("Size is: %d \n", size);
-  printf("Size is: %d \n", (int)sizeof(float));
+  printf("Size is: %d \n", (int)sizeof(long));
   return 0;
 }
