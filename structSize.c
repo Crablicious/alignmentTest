d48fec9cb0f2db518e6c2189739b42033a6df453
Adam <adwe@live.se>
2014-12-10 10:34:01 +0100

    test 4,5

--- structSize.c
+++ structSize.c
@@ -4,9 +4,19 @@
 #include <math.h>
 #include <string.h>
 
+size_t isLittle(void){
+  unsigned int x = 1;
+  return (int) (((char *)&x)[0]);
+}
+
+size_t max(size_t a, size_t b){
+  if(a>b) return a;
+  return b;
+}
 
 size_t structSizeFromString(char *layout) {
   size_t res = 0;
+  size_t align = 1; //Largest alignment.
   int i = 0;
   char c = *layout;
   while (c) {
@@ -16,23 +26,48 @@ size_t structSizeFromString(char *layout) {
       res += sizeof(char);
       break;
     case '*':
-      res += sizeof(void*) - res % sizeof(void*);
+      if(res % sizeof(void*) != 0){
+        res += sizeof(void*) - res % sizeof(void*);
+      }
+      align = max(align, sizeof(void*));
       res += sizeof(void*);
       break;
     case 'i':
+      if(res % sizeof(int) != 0){
+        res += sizeof(int) - res % sizeof(int);
+      }
+      align = max(align, sizeof(int));
       res += sizeof(int);
       break;
     case 'f':
+      if(res % sizeof(float) != 0){
+        res += sizeof(float) - res % sizeof(float);
+      }
+      align = max(align, sizeof(float));
       res += sizeof(float);
       break;
     case 'd':
+      if(isLittle() && res % sizeof(void*) != 0){
+        res += sizeof(void*) - res % sizeof(void*);
+        align = max(align, sizeof(void*));
+      }else if(res % sizeof(double) == 0){
+        res += sizeof(double) - res % sizeof(double);
+        align = max(align, sizeof(double));
+      }
       res += sizeof(double);
       break;
     case 'l':
+      if(res % sizeof(long) != 0){
+        res += sizeof(long) - res % sizeof(long);
+      }
+      align = max(align, sizeof(long));
       res += sizeof(long);
       break;
     }
     i++;
+  }
+  if(res % align != 0){
+    res += align - res % align;
   }
   return res;
 }
@@ -40,8 +75,12 @@ size_t structSizeFromString(char *layout) {
 int main(int argc, char *argv[])
 {
   char *string = malloc(10);
-  string = "c*";
+  string = "cdic*";
   size_t test = structSizeFromString(string);
+  printf("Size is: %d \n", (int)test);
+
+  string = "cdi";
+  test = structSizeFromString(string);
   printf("Size is: %d \n", (int)test);
   return 0;
 }
